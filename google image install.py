3f950aeccd09b6861fb1d89be917a2004b77ef5e
Kangnos <rkdtlsgur66@gmail.com>
2020-07-09 17:32:37 +0900

    2020 07 08

--- google image install.py
+++
@@ -1,8 +0,0 @@
-from google_images_download import google_images_download
-
-response = google_images_download.googleimagesdownload()
- 
-arguments = {"keywords" : "고양이", "limit" : 10, "print_urls" : True, "chromedriver": "chromedriver.exe"}
-
-paths = response.donwload(arguments)
-print(paths)
--- image_download.py
+++ image_download.py
@@ -7,16 +7,15 @@ baseUrl = 'https://search.naver.com/search.naver?where=image&sm=tab_jum&query='
 people = {'puppy': ['강다니엘', '백현', '박보검', '송중기'],
           'cat': ['황민현', '시우민', '이종석', '강동원', '이종석', '이준기'],
           'bear': ['마동석', '조진웅', '조세호', '안재홍'],
-          'dinosaur': ['윤두준', '이민기', '육성재', '공유', '김우빈'],
-          'rabbit': ['정국', '바비', '박지훈', '수호']}
-Path("./img").mkdir(parents=True, exist_ok=True)
+          'dinosaur': ['윤두준', '이민기', '육성재', '공유', '김우빈']}
+Path("./img").mkdir(parents=True, exist_ok=True) # img라는 디렉토리 생성
 for k, v in people.items():
     Path("./img/" + k).mkdir(parents=True, exist_ok=True) # 디렉토리 생성 함수 여기서 k는 people list의 key의 값이다. v는 peop list의 value 값이다
-    for person in v:
-        url = baseUrl + quote_plus(person) + " 얼굴" #qutoe_plus는 문자열을 url 구문으로 바꿔주는 함수이다
+    for person in v: # 뜻: v는 반복문에서 person이라는 변수로 사용하겠다 이소리
+        url = baseUrl + quote_plus(person) # qutoe_plus는 문자열을 url 구문으로 바꿔주는 함수이다
         html = urlopen(url)
         soup = bs(html, "html.parser")
-        img = soup.find_all(class_='_img', limit=30)
+        img = soup.find_all(class_='_img', limit=45)
         Path("./img/" + k + '/' + person).mkdir(parents=True, exist_ok=True) # 디렉토리 생성 함수
         n = 1
         for i in img:
